0f18b8568685598f1ee739db12aab8c1eace4e3b
Zon_206XLag <56264968+anhhackta@users.noreply.github.com>
2025-07-18 11:01:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/hoang-quangnam-portfolio-glow/', // << Thêm dòng này
   server: {
     host: "::",
     port: 8080,
